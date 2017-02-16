9ba067bf83bafacdf15e5da7f2c296bc0a479710
Zyquan Thomas <zthomas@wlapcs.org>
2017-02-16 21:17:46 +0000

    my first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function clearDisplay(){
+    document.getElementById("display-bar").innerHTML = ""; 
+}
+
+function display(userInput){
+    document.getElementById("display-bar").innerHTML += userInput;
+}
+
+function calculate(){
+    var answer = eval(document.getElementById("display-bar").innerHTML)
+    document.getElementById("display-bar").innerHTML = answer; 
+}
+
