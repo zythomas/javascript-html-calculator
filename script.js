841a5d767a6276e3e1f1990886032f1024b41c83
Zyquan Thomas <zthomas@wlapcs.org>
2017-02-22 11:03:08 +0000

    added text input so user can type

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function clearDisplay(){
-    document.getElementById("display-bar").innerHTML = ""; 
+    document.getElementById("display-bar").value = ""; 
 }
 
 function display(userInput){
-    document.getElementById("display-bar").innerHTML += userInput;
+    document.getElementById("display-bar").value += userInput;
 }
 
 function calculate(){
     var answer = eval(document.getElementById("display-bar").innerHTML)
-    document.getElementById("display-bar").innerHTML = answer; 
+    document.getElementById("display-bar").value = answer; 
 }
 
